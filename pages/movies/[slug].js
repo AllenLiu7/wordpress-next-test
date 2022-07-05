70883fa73ace49220da7e44da8b7efa4365d6619
Allen Liu <allen6qi@gmail.com>
2022-07-05 09:01:41 +0800

    minor change

--- pages/movies/[slug].js
+++ pages/movies/[slug].js
@@ -1,70 +1,71 @@
 import { gql } from '@apollo/client';
 import client from '../../apollo-client';
+import styles from './movie.module.css';
 
-function movieDetails({movie}){
-  return (
-    <>
-      <div>
-          Blog ID details pages
-          <pre>{JSON.stringify(movie, undefined, 2)}</pre>
-      </div>
-      <div dangerouslySetInnerHTML={{__html: movie.data.movieBy.content}}></div>
-    </>
-  )
+function movieDetails({ movie }) {
+    return (
+        <>
+            <div>
+                Blog ID details pages
+                <pre>{JSON.stringify(movie, undefined, 2)}</pre>
+            </div>
+            <div className='container'>
+                <div  className={styles.content}
+                    dangerouslySetInnerHTML={{
+                        __html: movie.data.movieBy.content,
+                    }}
+                ></div>
+            </div>
+        </>
+    );
 }
 
-export default movieDetails
-
-
+export default movieDetails;
 
 export async function getStaticPaths() {
-  const result = await client.query({
-    query: gql`
-    query MoviePages {
-      movies {
-        nodes {
-          slug
-          id
-        }
-      }
-    }
-    `,
-});
+    const result = await client.query({
+        query: gql`
+            query MoviePages {
+                movies {
+                    nodes {
+                        slug
+                        id
+                    }
+                }
+            }
+        `,
+    });
 
-return {
-  paths: result.data.movies.nodes.map(({slug}) => {
     return {
-      params: {slug}
-    }
-  }),
-  fallback: false
-}
+        paths: result.data.movies.nodes.map(({ slug }) => {
+            return {
+                params: { slug },
+            };
+        }),
+        fallback: false,
+    };
 }
 
 export async function getStaticProps({ params }) {
-  const { slug } = params;
+    const { slug } = params;
 
-  const result = await client.query({
-    query: gql`
-    query NewQuery ($slug: String!){
-      movieBy(slug: $slug) {
-        id
-        title
-        content
-        slug
-      }
-    }
-    `,
-    variables: {slug}
-});
+    const result = await client.query({
+        query: gql`
+            query NewQuery($slug: String!) {
+                movieBy(slug: $slug) {
+                    id
+                    title
+                    content
+                    slug
+                }
+            }
+        `,
+        variables: { slug },
+    });
 
-  return{
-    props: {
-      movie: result
-    }
-  }
+    return {
+        props: {
+            movie: result,
+        },
+    };
 }
-
-
-
-
